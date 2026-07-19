d6c427388be72b3e0e6d779683f87b42d5081762
mr9asif <asifalibd002@gmail.com>
2026-07-19 19:20:42 +0600

    footer setting working dropdown added

--- frontend/src/components/sidebar/footer/FooterAction.tsx
+++ frontend/src/components/sidebar/footer/FooterAction.tsx
@@ -1,17 +1,43 @@
-import { Settings } from "lucide-react";
+import { LogOut, Settings } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const FooterActions = () => {
   return (
-    <button
-      className="
-      rounded-md
-      p-2
-      transition
-      hover:bg-accent
-      "
-    >
-      <Settings className="h-4 w-4" />
-    </button>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <button
+          className="
+            rounded-md
+            p-2
+            transition
+            hover:bg-accent
+          "
+        >
+          <Settings className="h-4 w-4" />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-52">
+        <DropdownMenuItem>
+          <Settings className="mr-2 h-4 w-4" />
+          Preferences
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className="text-destructive">
+          <LogOut className="mr-2 h-4 w-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
